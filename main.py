30fbef29ba5a6da05663d5cb7631c4fc959598c6
--replace-all <renfredreuben@gmail.com>
2023-10-13 13:22:23 +0100

    added python file

---
+++ main.py
@@ -0,0 +1,28 @@
+import subprocess
+
+def run_ffuf(target_url, wordlist_path, output_dir):
+    try:
+        # Define the ffuf command
+        ffuf_command = [
+            "ffuf",
+            "-u", target_url,
+            "-w", wordlist_path,
+            "-o", output_dir,
+            "-t", "1",  # Number of threads (adjust as needed)
+            "-c",       # Colorize the output
+        ]
+
+        # Execute the ffuf command
+        subprocess.run(ffuf_command, check=True)
+
+    except subprocess.CalledProcessError as e:
+        print(f"Error running ffuf: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+if __name__ == "__main__":
+    target_url = "http://example.com/FUZZ"  # Replace with your target URL
+    wordlist_path = "wordlist.txt"         # Replace with the path to your wordlist file
+    output_dir = "ffuf_output"             # Replace with the desired output directory
+
+    run_ffuf(target_url, wordlist_path, output_dir)
